0ca07f3ef86247ee7643e685ad5ba819744e0e62
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-05-16 22:24:09 +0900

    update

--- main.go
+++ main.go
@@ -20,16 +20,13 @@ var words []string
 //go:embed words.txt
 var wordlist string
 
-func isSomeWords(s string, m int) bool {
+func isSomeWords(s string) bool {
 	for _, w := range words {
-		if len(w) < m {
-			continue
-		}
 		if s == w {
 			return true
 		}
-		if strings.HasPrefix(s, w) {
-			if isSomeWords(s[len(w):], m) {
+		if len(w) < len(s) && strings.HasPrefix(s, w) {
+			if isSomeWords(s[len(w):]) {
 				return true
 			}
 		}
@@ -56,6 +53,11 @@ func main() {
 	flag.Parse()
 
 	words = strings.Split(wordlist, "\n")
+	for i := 0; i < len(words); i++ {
+		if len(words[i]) == 0 {
+			words = words[:i+copy(words[i:], words[i+1:])]
+		}
+	}
 	sort.Slice(words, func(i, j int) bool {
 		return len(words[i]) > len(words[j])
 	})
@@ -66,7 +68,7 @@ func main() {
 		s := scanner.Text()
 		if !unicode.IsLetter(rune(s[0])) {
 			fmt.Fprint(color.Output, s)
-		} else if len(s) < min || isSomeWords(strings.ToLower(s), min) {
+		} else if len(s) < min || isSomeWords(strings.ToLower(s)) {
 			fmt.Fprint(color.Output, s)
 		} else if v := maybeTypo(s); v != 0 && v > 0.4 {
 			fmt.Fprint(color.Output, s)
